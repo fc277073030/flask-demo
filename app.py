8e97ebaeb6443844bdec4b76b392f0596d8aa738
阿超 <fc277073030@users.noreply.github.com>
2019-10-07 10:42:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! fanchao test1,test2,k8s,at home,an test again,test jenkins'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
